c8eb221c82c2f8669676d44dee63fa0255cf1495
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2026-04-24 00:18:38 +0300

    imlemented create passwrod, and update password operations and fixed old errors in manager

--- modules/password_manager/manager.py
+++ modules/password_manager/manager.py
@@ -1,19 +1,20 @@
-from uuid import UUID
-from typing import Optional, Sequence
+from typing import Optional, Sequence, Any
 
-from sqlalchemy import select
+from sqlalchemy import func
+from sqlalchemy import select, update
 from sqlalchemy.exc import NoResultFound
 
 from .settings import Session
 from .models import PasswordModel
 
 
-def create_password():
+def create_password(password_data: dict[str, Any]) -> None:
     with Session() as session, session.begin():
-        pass
+        new_record = PasswordModel(**password_data)
+        session.add(new_record)
 
 
-def get_password(password_id: UUID) -> PasswordModel | None:
+def get_password(password_id: str) -> PasswordModel | None:
     with Session() as session:
         return session.get(PasswordModel, password_id)
 
@@ -22,22 +23,31 @@ def get_passwords_list(
     ordering_by: str = "name", name: Optional[str] = None
 ) -> Sequence[PasswordModel]:
     with Session() as session:
+        order_column = getattr(PasswordModel, ordering_by, PasswordModel.name)
         if not name:
-            return session.scalars(select(PasswordModel).order_by(ordering_by)).all()
+            return session.scalars(select(PasswordModel).order_by(order_column)).all()
         return session.scalars(
             select(PasswordModel)
-            .where(PasswordModel.name.icontains(name))
-            .order_by(ordering_by)
+            .where(func.lower(PasswordModel.name).contains(name.lower()))
+            .order_by(order_column)
         ).all()
 
 
-def update_password():
-    pass
-
-
-def delete_password(password_id: UUID) -> None:
+def update_password(to_update: dict[str, Any]) -> None:
     with Session() as session, session.begin():
-        record = get_password(password_id)
+        data = to_update.copy()
+        record_id = data.pop("id")
+
+        session.execute(
+            update(PasswordModel)
+            .where(PasswordModel.id == record_id)
+            .values(**data)
+        )
+
+
+def delete_password(password_id: str) -> None:
+    with Session() as session, session.begin():
+        record = session.get(PasswordModel, password_id)
         if not record:
             raise NoResultFound("Password not found")
         session.delete(record)
